c0906037e5d05bb3b4da618990547f9058df6b2d
DEL PUP <samveldelpup@gmail.com>
2021-04-10 07:38:26 +0200

    mise à jour

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pygame
 
 class Piece:
+    nom=""
     def __init__(self, pos, camp):
         self.pos = pos
         self.camp = camp
         self.has_moved = False
+        if camp==1:
+            self.couleur="blanc"
+        else:
+            self.couleur="noir"
 
     def get_pos(self):
         return self.pos
@@ -14,49 +19,28 @@ class Piece:
 
     def move(self, pos):
         if pos in self.premove():
-            if game.find_case(pos) == 0:
-                self.pos = pos
-            else:
-                tab=player1.get_piece() + player2.get_piece()
-                i=0
-                while tab[i].get_pos()!=pos:
-                    i+=1
-
-                if tab[i].camp!=self.camp:
-                    self.pos = pos
-
-
-
+            self.pos=pos
+            return True
+        return False
+    
+    def dessine(self):
+        window_surface.blit(pieces_dict[self.nom+'_'+self.couleur], [(self.get_pos()[1]-1)*ratio, abs((self.get_pos()[0]-8)*ratio)])
 
 
 class Cavalier(Piece):
-    def __init__(self, pos, camp):
-        super().__init__(pos, camp)
+    nom="cavalier"
 
     def premove(self):
         tab = [(self.pos[0]+2, self.pos[1]+1), (self.pos[0]+1, self.pos[1]+2), (self.pos[0]-2, self.pos[1]-1), (self.pos[0]-1, self.pos[1]-2), (self.pos[0]+1, self.pos[1]-2), (self.pos[0]+2, self.pos[1]-1), (self.pos[0]-2, self.pos[1]+1),  (self.pos[0]-1, self.pos[1]+2)]
         tab= game.check_limit(tab)
+        for pos in tab:
+            if game.find_case(pos)==self.camp:
+                tab.remove(pos)
         return tab
     
 
 class Tour(Piece):
-    def __init__(self, pos, camp):
-        super().__init__(pos, camp)
-    
-    '''
-    def premove(self):
-        tab=[]
-        for i in range(1, 8):
-            
-                tab.append((self.pos[0]+i, self.pos[1]))
-                tab.append((self.pos[0], self.pos[1]+i))
-        return tab
-
-    '''
-    def dessine(self):
-        if self.camp==2:
-            window_surface.blit(tour_noir, [(self.get_pos()[1]-1)*ratio, abs((self.get_pos()[0]-8)*ratio)])
-        
+    nom="tour"           
 
     def premove(self):
         tab=[]
@@ -65,6 +49,8 @@ class Tour(Piece):
             i=1
             new_pos=(self.pos[0] + i * direction[0], self.pos[1] + i * direction[1])            
             while game.check_limit([new_pos]) != []:
+                if game.find_case(new_pos)==self.camp:
+                    break
                 if game.find_case(new_pos)!=0:
                     tab.append(new_pos)
                     break                      
@@ -77,24 +63,8 @@ class Tour(Piece):
 
 
 class Fou(Piece):
-    def __init__(self, pos, camp):
-        super().__init__(pos, camp)
+    nom="fou"
     
-    def dessine(self):
-        window_surface.blit(fou_noir, [(self.get_pos()[1]-1)*ratio, abs((self.get_pos()[0]-8)*ratio)])
-
-    '''
-    def premove(self):
-        tab=[]
-        for i in range(-8, 8):
-            if i!=0:
-                tab.append((self.pos[0]+i, self.pos[1]+i))
-                tab.append((self.pos[0]+i, self.pos[1]-i))
-                tab.append((self.pos[0]-i, self.pos[1]+i))
-                tab.append((self.pos[0]-i, self.pos[1]-i))
-        return tab
-
-    '''
     def premove(self):
         tab=[]
         
@@ -113,35 +83,27 @@ class Fou(Piece):
 
 
 class Reine(Piece):
-    def __init__(self, pos, camp):
-        super().__init__(pos, camp)
-    
-    '''
+    nom="reinne"
+
     def premove(self):
         tab=[]
-        for i in range(-8, 8):
-            if i!=0:
-                tab.append((self.pos[0]+i, self.pos[1]+i))
-                tab.append((self.pos[0]+i, self.pos[1]-i))
-                tab.append((self.pos[0]-i, self.pos[1]+i))
-                tab.append((self.pos[0]-i, self.pos[1]-i))
-                tab.append((self.pos[0]+i, self.pos[1]))
-                tab.append((self.pos[0], self.pos[1]+i))  
-        return tab
-    '''
-    def premove(self):
-        tab=[]
-            
+        
         for direction in fou_directions+tour_directions :
             i=1
-            while game.check_limit([self.pos[0] + i * direction[0], self.pos[1] + i * direction[1]]) != [] and not game.find_case((self.pos[0] + i * direction[0], self.pos[1] + i * direction[1])):
-                tab.append((self.pos[0], self.pos[1]+i))
+            new_pos=(self.pos[0] + i * direction[0], self.pos[1] + i * direction[1])            
+            while game.check_limit([new_pos]) != []:
+                if game.find_case(new_pos)!=0:
+                    tab.append(new_pos)
+                    break                      
+                tab.append(new_pos)
                 i+=1
+                new_pos=(self.pos[0] + i * direction[0], self.pos[1] + i * direction[1])
+
         return tab
 
+
 class Roi(Piece):
-    def __init__(self, pos, camp):
-        super().__init__(pos, camp)
+    nom="roi"
 
     def premove(self):
         tab=[]
@@ -154,8 +116,7 @@ class Roi(Piece):
 ###ALED ON VERRA APRèS
 
 class Pion(Piece):
-    def __init__(self, pos, camp):
-        super().__init__(pos, camp)
+    nom="pion"
 
     def premove(self):
         tab=[]
@@ -182,9 +143,9 @@ class Pion(Piece):
 
 class Game:
     def __init__(self):
-        player2.add_piece(Tour((6,2), 2))
-        player2.add_piece(Fou((2,6), 2))
-        '''
+        #player2.add_piece(Tour((6,2), 2))
+        #player2.add_piece(Fou((2,6), 2))
+        
         player1.add_piece(Reine((1,4), 1))
         player2.add_piece(Reine((8,4), 2))
 
@@ -209,7 +170,17 @@ class Game:
             player1.add_piece(Pion((2,i), 1))
             player2.add_piece(Pion((7,i), 2))
 
-    '''
+    
+        for x in image_pieces:
+            piece_blanc = pygame.image.load('assets/{}_blanc.png'.format(x))
+            piece_noir = pygame.image.load('assets/{}_noir.png'.format(x))
+
+            piece_blanc.convert_alpha()
+            piece_noir.convert_alpha()
+
+            pieces_dict['{}_blanc'.format(x)] = pygame.transform.scale(piece_blanc, [ratio//2, ratio//2])
+            pieces_dict['{}_noir'.format(x)] = pygame.transform.scale(piece_noir, [ratio//2, ratio//2])
+        print(pieces_dict)
 
     def check_limit(self, tab):
         tab_check=[]
@@ -226,9 +197,6 @@ class Game:
                 return piece.get_camp()
         return 0
      
-        
- 
-
 
 class Player:
     def __init__(self, pseudo, color):
@@ -249,24 +217,21 @@ player2= Player("Noémie", 2)
 
 tour_directions=[(0, 1), (1, 0), (-1, 0), (0, -1)]
 fou_directions=[(1, 1), (1, -1), (-1, 1), (-1, -1)]
+pieces_dict = {}
+image_pieces = ['roi', 'reinne', 'pion', 'fou', 'tour', 'cavalier']
+resolution_x = 560
+resolution_y = 560
+damier = pygame.image.load('assets/damier.jpg')
+
+ratio = 560//8
+window_surface=pygame.display.set_mode((resolution_x, resolution_y))
 
 game = Game()
 
 print(player1.get_piece())
 
-resolution_x = 560
-resolution_y = 560
 
-ratio = 560//8
-window_surface=pygame.display.set_mode((resolution_x, resolution_y))
 
-damier = pygame.image.load("assets/damier.jpg")
-tour_noir = pygame.image.load("assets/tour_noir.png")
-tour_noir.convert_alpha()
-tour_noir = pygame.transform.scale(tour_noir, [ratio//2, ratio//2])
-fou_noir = pygame.image.load("assets/fou_noir.png")
-fou_noir.convert_alpha()
-fou_noir = pygame.transform.scale(fou_noir, [ratio//2, ratio//2])
 launched=True
 piece_selected = None
 while launched:
@@ -292,7 +257,7 @@ while launched:
                 piece_selected=None
         else:
             window_surface.blit(damier, [0, 0])
-            for piece in player2.get_piece():
+            for piece in player2.get_piece()+player1.get_piece():
                 piece.dessine()
                 print([(piece.get_pos()[1]-1)*ratio, abs((piece.get_pos()[0]-8)*ratio)])
                 
